594d209d878861fe8ff4feab96f5446771500511
Michael Weir <weirm@mail.gvsu.edu>
2019-04-05 18:25:30 -0400

    initial ai steps

--- snakeAI.py
+++ snakeAI.py
@@ -4,6 +4,18 @@ import time
 import random
 import math
 
+import gym
+import numpy as np
+import tflearn.layers.core import input_layer, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import mean, median
+from collections import Counter
+
+LR = 1e-3
+goal_steps = 300
+score_requirement = 50
+initial_games = 5000
+
 
 
 

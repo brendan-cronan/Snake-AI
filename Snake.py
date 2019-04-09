fc8a563e7cd529444b21e77a8ecb16e4cc5cbd01
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2019-04-09 19:49:13 -0400

    Added comments for Snake.py

--- Snake.py
+++ Snake.py
@@ -8,10 +8,17 @@ BOARD_CELL_NUMBER = 10
 
 
 class Snake:
+    """
+    Initializes the important variables in the constructor
+    """
     def __init__(self):
+        #location of the head of the snake.  Starts at the middle.
         self.head = [BOARD_CELL_NUMBER/2,BOARD_CELL_NUMBER/2]
+        #List of locations that it has occupied in the last [length] turns.
         self.tail = []
+        #List of its *most recent* direction to avoid going "backwards"
         self.facing = [0,0]
+        #boolean flag to determine whether to remove a value from self.tail
         self.growing=False
 
 
@@ -23,25 +30,30 @@ class Snake:
     Return "GAME_OVER" if the movement is out of bounds
     """
     def move(self, direction):
-        if self.check_facing(direction):
-            out_of_bounds = self.check_bounds(self.get_position_from_current(direction), BOARD_CELL_NUMBER)
-            if out_of_bounds:
-                self._move_snake(direction)
-                return True
+        #Call helper method to check that move is not trying to go backwards
+        if self.check_facing(direction):#if not going backwards...
+            #Method call to check if position in desired direction falls outside game bounds
+            inside_bounds = self.check_bounds(self.get_position_from_current(direction), BOARD_CELL_NUMBER)
+            if inside_bounds:#if inside the game bounds...
+                self._move_snake(direction)#call private helper method to execute movement.
+                return True#return true to indicates successful move
             else:
-                return "GAME_OVER"
+                return "GAME_OVER"#Only return Game over if it is out of bounds.
+            #if it is merely trying to go backwards, return nothing.
 
 
 
 
     """
-    get the position from the given position in the given direction
+    Get a new position from the given location in the given direction
     """
     def get_position(self, position, direction):
-        new_pos = [ position[0]+direction[0] , position[1]+direction[1] ]
-        return new_pos
+        #merely returns the position with the direction added to it.
+        return [ position[0]+direction[0] , position[1]+direction[1] ]
+
     """
-    get the position from the current Snake position in the given direction
+    Get the position from the Current Snake Position in the given direction
+    Just calls the get_position with the current location for convenience.
     """
     def get_position_from_current(self, direction):
         return self.get_position(self.head,direction)
@@ -60,7 +72,7 @@ class Snake:
 
 
     """
-    This method takes a direction --> [-1,0]
+    This method takes a direction ex. --> [-1,0]
     and sets the snake's position to the tile in this direction
 
     This PRIVATE method assumes that the position is valid and has gone through all checks.
@@ -68,24 +80,26 @@ class Snake:
     def _move_snake(self, direction):
 
 
+        #add position to the tail before the movement.
         self.tail.insert(0, list(self.head))
 
+        #actually change the position of the head
         self.head[0] += direction[0]
         self.head[1] += direction[1]
 
-
-
-
+        #Set the facing to the "last" direction
         self.facing = direction
 
+        #only pop off the last position in tail if it hasn't eaten this cycle.
         if not self.growing:
             self.tail.pop()
-        else:
+        else:#merely set the flag back to false to "grow"
             self.growing = False
 
 
     """
     Call this function when the snake eats a fruit.
+    Called by the Game class.
     """
     def grow(self):
         self.growing = True
@@ -97,14 +111,16 @@ class Snake:
         or the same as the opposite of the old direction.
     """
     def check_facing(self, direction):
+        #opposite direction of the current facing is not allowed.
         opposite_dir = [self.facing[0]*-1, self.facing[1]*-1]
         return not direction == opposite_dir
 
     """
     Return True if the new_position collides with given list of points
+    Return nothing if no collisions
     """
     def check_collisions(self, new_position, collision_positions):
-        collisions = [ True for loc in collision_positions if new_position == loc ]
-        for flag in collisions:
-            if flag:
-                return flag;
+        #list comprehension to make a True value if the provided position is equal to any value in loc.
+        collisions = [ 1 for loc in collision_positions if new_position == loc ]
+        if 1 in collisions:#loop through and return True if any 1 values are present
+            return True

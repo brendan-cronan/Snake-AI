00005aa22c0f210af16cd54a5a57fab362e1aba0
michaelweir14 <35287488+michaelweir14@users.noreply.github.com>
2019-04-09 15:22:19 -0400

    Update player.py

--- player.py
+++ player.py
@@ -20,7 +20,7 @@ score_requirement = 50
 initial_games = 500
 
 def some_random_games_first():
-    # Each of these is its own game.
+
     for episode in range(10):
 
         env = game()
@@ -28,15 +28,15 @@ def some_random_games_first():
         first = True
         for _ in range(goal_steps):
 
-            # action = random.randrange(0, 3)
+     
             action = random.randrange(0, 4)
-            # action = 2
+      
 
             if first:
                 first = False
                 action = 2
 
-            # do it! render the previous view
+  
             env.render()
             observation, reward, done, info = env.step(action)
             a = 0
@@ -44,29 +44,29 @@ def some_random_games_first():
 
 
 def generate_population(model):
-    # [OBS, MOVES]
+  
     global score_requirement
 
     training_data = []
-    # all scores:
+
     scores = []
-    # just the scores that met our threshold:
+ 
     accepted_scores = []
-    # iterate through however many games we want:
+   
     print('Score Requirement:', score_requirement)
     for _ in range(initial_games):
         print('Simulation ', _, " out of ", str(initial_games), '\r', end='')
-        # reset env to play again
+     
         env.reset()
 
         score = 0
-        # moves specifically from this environment:
+      
         game_memory = []
-        # previous observation that we saw
+     
         prev_observation = []
-        # for each frame in 200
+     
         for _ in range(goal_steps):
-            # choose random action (0 or 1)
+        
             if len(prev_observation) == 0:
                 action = random.randrange(0, 4)
             else:
@@ -76,64 +76,52 @@ def generate_population(model):
                     prediction = model.predict(prev_observation.reshape(-1, len(prev_observation), 1))
                     action = np.argmax(prediction[0])
 
-            # do it!
+        
             observation, reward, done, info = env.step(action)
 
-            # notice that the observation is returned FROM the action
-            # so we'll store the previous observation here, pairing
-            # the prev observation to the action we'll take.
+       
             if len(prev_observation) > 0:
                 game_memory.append([prev_observation, action])
             prev_observation = observation
             score += reward
             if done: break
 
-        # IF our score is higher than our threshold, we'd like to save
-        # every move we made
-        # NOTE the reinforcement methodology here.
-        # all we're doing is reinforcing the score, we're not trying
-        # to influence the machine in any way as to HOW that score is
-        # reached.
         if score >= score_requirement:
             accepted_scores.append(score)
             for data in game_memory:
-                # convert to one-hot (this is the output layer for our neural network)
+                # output layer
                 
                 action_sample = [0, 0, 0, 0]
-                    #print("hi")
-                    # print(data[1])
+         
                 action_sample[data[1]] = 1
                 output = action_sample
                 # saving our training data
                 training_data.append([data[0], output])
 
-        # save overall scores
+ 
         scores.append(score)
 
-    # some stats here, to further illustrate the neural network magic!
+
     print('Average accepted score:', mean(accepted_scores))
     print('Score Requirement:', score_requirement)
     print('Median score for accepted scores:', median(accepted_scores))
     print(Counter(accepted_scores))
     score_requirement = mean(accepted_scores)
 
-    # just in case you wanted to reference later
+
     training_data_save = np.array([training_data, score_requirement])
     np.save('saved.npy', training_data_save)
 
     return training_data
 
 
-def create_dummy_model(training_data):
+def create_initial_model(training_data):
     shape_second_parameter = len(training_data[0][0])
     x = np.array([i[0] for i in training_data])
     X = x.reshape(-1, shape_second_parameter, 1)
     y = [i[1] for i in training_data]
     model = create_neural_network_model(input_size=len(X[0]), output_size=len(y[0]))
-#    print("here")
-#    print(len(X[0]))
-#    print(len(y[0]))
-    #len(X[0]) ,
+
     return model
 
 
@@ -154,9 +142,7 @@ def train_model(training_data, model=False):
     
     X = x.reshape(-1, shape_second_parameter, 1)
     y = [i[1] for i in training_data]
-#    print(x)
-#    print(X)
-#    print(y)
+
 
     model.fit({'input': X}, {'targets': y}, n_epoch=10, batch_size=16, show_metric=True)
     model.save('snake_trained.tflearn')
@@ -166,7 +152,7 @@ def train_model(training_data, model=False):
 
 
 def evaluate(model):
-    # now it's time to evaluate the trained model
+  
     scores = []
     choices = []
     for each_game in range(10):
@@ -204,29 +190,19 @@ if __name__ == "__main__":
     some_random_games_first()
     # initial_population
     training_data = generate_population(None)
-    # creating a dummy model
-    model = create_dummy_model(training_data)
+    # creating a initial model
+    model = create_initial_model(training_data)
     # training with first dataset
     model = train_model(training_data, model)
-
-
     # evaluating
     evaluate(model)
-#   model.load('saved.npy')
-#
-#
-#
-#
-#
-#
-#
+
     #     recursive learning
     generation = 1
     while generation < 3:
         generation += 1
 
         print('Generation: ', generation)
-        # training_data = initial_population(model)
         training_data = np.append(training_data, generate_population(None), axis=0)
         print('generation: ', generation, ' initial population: ', len(training_data))
         if len(training_data) == 0:

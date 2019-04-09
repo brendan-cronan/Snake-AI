e43878373b0758e94b3b2c38b1930ebdfc640b3c
Michael Weir <weirm@mail.gvsu.edu>
2019-04-09 18:01:04 -0400

    fixed indent

--- player.py
+++ player.py
@@ -216,7 +216,7 @@ def evaluate(model):
         scores.append(score)
 #     print('Average Score is')
 #     print('Average Score:', sum(scores) / len(scores))
-     print('choice 1:{}  choice 0:{}'.format(choices.count(1) / len(choices), choices.count(0) / len(choices)))
+    print('choice 1:{}  choice 0:{}'.format(choices.count(1) / len(choices), choices.count(0) / len(choices)))
     print('Score Requirement:', score_requirement)
 
 #main method to call each function.
